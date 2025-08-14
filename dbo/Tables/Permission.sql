66016ffee92c0c354db355a6834339a5833901aa
jlinaresm25 <jlinarme@emeal.nttdata.com>
2025-08-14 00:47:07 -0500

    Add project files.

---
+++ dbo/Tables/Permission.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[permission]
+(
+	[id]                    BIGINT IDENTITY(1,1) NOT NULL,
+    [employee_first_name]   VARCHAR(100) NOT NULL,
+    [employee_last_name]    VARCHAR(100) NOT NULL,
+    [permission_type_id]    INT          NOT NULL,
+    [permission_date]       DATE         NOT NULL,
+
+    PRIMARY KEY CLUSTERED ([id] ASC),
+    FOREIGN KEY ([permission_type_id]) REFERENCES [dbo].[permission_type] ([id])
+)
---
+++ dbo/Tables/permission_type.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[permission_type]
+(
+	[id]			INT IDENTITY(1,1)	NOT NULL,
+    [description]	VARCHAR(100)		NOT NULL,
+
+	PRIMARY KEY CLUSTERED ([id] ASC)
+)
