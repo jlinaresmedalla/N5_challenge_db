eccda8ce3b6c85db81edbd46af9a1177a214b011
jlinaresm25 <jlinarme@emeal.nttdata.com>
2025-08-15 00:05:10 -0500

    feat: Add initial data

---
+++ Script.PostDeployment1.sql
@@ -0,0 +1,11 @@
+﻿IF EXISTS (SELECT 1 FROM sys.tables WHERE name = 'permission_type' AND schema_id = SCHEMA_ID('dbo')) AND NOT EXISTS (SELECT 1 FROM [dbo].[permission_type])
+BEGIN
+    INSERT INTO [dbo].[permission_type] ([description]) VALUES
+    ('READ'),
+    ('WRITE'),
+    ('DELETE'),
+    ('ADMIN'),
+    ('EXPORT'),
+    ('VIEW_REPORTS'),
+    ('MANAGE_USERS');
+END
